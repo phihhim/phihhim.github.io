f687e99a879df74f13e53c7fd741f1d78f54174c
an.vu1 <an.vu@vti.com.vn>
2023-05-03 11:40:35 +0700

    first

---
+++ content/SICP/first.md
@@ -0,0 +1,7 @@
+Title: My First Review
+Date: 2010-12-03 10:20
+Category: SICP
+Summary: Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl.
+
+
+Following is a review of my favorite mechanical keyboard.
---
+++ pelicanconf.py
@@ -0,0 +1,58 @@
+AUTHOR = 'phihhim'
+SITENAME = 'phihhim'
+SITEURL = ''
+SUBTITLE = 'Phihhim'
+SUBTEXT = '<i>This site is dedicated, in respect and admiration, to the spirit that lives in the computer.</i>'
+PATH = 'content'
+
+TIMEZONE = 'Asia/Ho_Chi_Minh'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'themes/Papyrus'
+THEME_STATIC_PATHS = ['static']
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['readtime', 'search', 'neighbors', 'pelican-toc']
+
+DISPLAY_PAGES_ON_MENU = True
+DIRECT_TEMPLATES = (('index', 'search', 'tags', 'categories', 'archives',))
+PAGINATED_TEMPLATES = {'index':None,'tag':None,'category':None,'author':None,'archives':24,}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+SEARCH_MODE = "output"
+SEARCH_HTML_SELECTOR = "main"
+# Table of Content Plugin
+TOC = {
+    'TOC_HEADERS'       : '^h[1-3]', # What headers should be included in
+                                     # the generated toc
+                                     # Expected format is a regular expression
+    'TOC_RUN'           : 'true',    # Default value for toc generation,
+                                     # if it does not evaluate
+                                     # to 'true' no toc will be generated
+    'TOC_INCLUDE_TITLE': 'false',    # If 'true' include title in toc
+}
+
+
+
